ea70d473df18f9ee0691444321abd5aee086ad17
jimmi187 <project.jimi@proton.me>
2023-01-25 22:51:07 +0100

    done maven multy module build

--- pet-clinic-data/src/main/java/com/jimi/springpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/jimi/springpetclinic/model/Person.java
@@ -1,10 +1,21 @@
 package com.jimi.springpetclinic.model;
 
+
+
+
 public class Person {
     private String firstName;
     private String lastName;
 
 
+    public Person(){
+
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
     public String getFirstName() {
         return firstName;
---
+++ pet-clinic-web/src/main/java/com/jimi/springpetclinic/SpringPetClinicApplication.java
@@ -0,0 +1,19 @@
+package com.jimi.springpetclinic;
+
+import com.jimi.springpetclinic.model.Person;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringPetClinicApplication {
+
+	public static void main(String[] args) {
+
+		ApplicationContext context = SpringApplication.run(SpringPetClinicApplication.class, args);
+		Person p = new Person("dzimi", "dzimic");
+		System.out.println(p.getFirstName());
+
+	}
+
+}
