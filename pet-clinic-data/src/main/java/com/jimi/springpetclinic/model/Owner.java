6891212b7333e490ceb0b7b3f77ba0471982f112
jimmi187 <project.jimi@proton.me>
2023-01-28 15:39:39 +0100

    added Visit.java and update Owner with reference to Pet

--- pet-clinic-data/src/main/java/com/jimi/springpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/jimi/springpetclinic/model/Owner.java
@@ -1,6 +1,15 @@
 package com.jimi.springpetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
+    private Set<Pet> pers;
 
+    public Set<Pet> getPers() {
+        return pers;
+    }
 
+    public void setPers(Set<Pet> pers) {
+        this.pers = pers;
+    }
 }
