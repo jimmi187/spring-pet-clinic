e530f4c9b3586561d6afba54212769258106dc09
jimmi187 <project.jimi@proton.me>
2023-01-28 18:29:19 +0100

    taking care of specialty ids

--- pet-clinic-data/src/main/java/com/jimi/springpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/jimi/springpetclinic/services/map/VetServiceMap.java
@@ -1,6 +1,8 @@
 package com.jimi.springpetclinic.services.map;
 
+import com.jimi.springpetclinic.model.Speciality;
 import com.jimi.springpetclinic.model.Vet;
+import com.jimi.springpetclinic.services.SpecialityService;
 import com.jimi.springpetclinic.services.VetService;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,12 @@ import java.util.Set;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+
+    private final SpecialityService specialityService;
+
+    public VetServiceMap(SpecialityService specialityService) {
+        this.specialityService = specialityService;
+    }
 
     @Override
     public Set<Vet> findAll() {
@@ -21,6 +29,12 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet object) {
+        if (object.getSpecialities().size() > 0){
+            object.getSpecialities().forEach( speciality -> {
+                Speciality savedSpec = specialityService.save(speciality);
+                speciality.setId(savedSpec.getId());
+            });
+        }
         return super.save( object);
     }
 
